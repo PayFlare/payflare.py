3d30fe7f1fc869e39a534e4054394f078be3a1df
git <test@example.com>
2022-09-24 19:38:48 +0200

    v2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="payflare",
-    version="0.0.1",
+    version="0.0.2",
     author="Payflare",
     description="Payflare.io Python Client Library",
     packages=["payflare"],
